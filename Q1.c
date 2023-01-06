ce7fcc0a849d6b3b45fa374b677db5753e569ad8
ChandraShekharSaini <112823190+ChandraShekharSaini@users.noreply.github.com>
2023-01-06 23:39:35 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    
+    int n,m;
+    
+    printf("Enter number1:");
+    scanf("%d",&n);
+    printf("Enter number2:");
+    scanf("%d",&m);
+
+    printf("Addition of two numbers: %d\n",m+n);
+    printf("Subtraction of two number:%d\n",n-m);
+    printf("Multiplication of two number:%d\n",m*n);
+    printf("Division of two number:%d\n",n/m);
+    
+    return 0;
+}
